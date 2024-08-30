d54391de08ac4bf02445502778586633044947c8
Bagus Dwi Putra <bagusdp2011@gmail.com>
2024-08-30 14:54:33 +0700

    First initialize success

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+/**
+ * Link
+ */
+export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20220911T010000Z%2F20220911T080000Z&text=Arin+%26+Miftah+Wedding&details=Arin+%26+Miftah+Wedding`;
+export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/kfFTBJKwRhw3qWut8`;
+
+/**
+ * Wedding time
+ */
+export const EPOCH_START_EVENT = 1754028000;
+export const EPOCH_END_EVENT = 1754049600;
+export const UTC_WEDDING_TIME = '2025-08-01:04:00:00Z';
---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const test = () => (
+  <div>
+    <h1>Welcome to the testpage!</h1>
+    {/* Other components and content */}
+  </div>
+);
+
+export default test;
